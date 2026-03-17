a5e270ae1650b8f7328f5ee4280f3f6f5437715c
deepikanc236 <deepika@example.com>
2026-03-17 14:44:28 +0530

    Move check-roles to root urls.py to bypass 404 issue

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from leads import views as lead_views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dashboard/", include("leads.dashboard_urls")),
+    path("check-roles/", lead_views.check_roles_debug, name='check_roles_debug_root'),
     path("", include("leads.urls")),
 ]
