0793a6415c5dc1403d6d1dd6e35a4004df98bde0
deepikanc236 <deepika@example.com>
2026-03-17 11:00:01 +0530

    Automate superuser creation in build script

--- render-build.sh
+++ render-build.sh
@@ -11,3 +11,11 @@ fi
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser automatically if environment variables are set
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL || true
+fi
